07ed0b27f17438d1f88ec08e76a3aaeb064bd260
Vid <vid_github@zooid.org>
2022-12-06 13:25:22 -0500

    cleanup and package

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  moduleFileExtensions: ['js', 'json', 'vue'],
-}
